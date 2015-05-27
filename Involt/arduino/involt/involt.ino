4f796b98a1098372382534617def5617ff787dcb
CoryWilson <ctwilson08@gmail.com>
2015-05-26 23:41:51 -0400

    Hub with Ethernet Shield Works
    Switched pins to 6,7

--- Involt/arduino/involt/involt.ino
+++ Involt/arduino/involt/involt.ino
@@ -23,12 +23,39 @@ boolean directMode = false;
   IMPORTANT: You need to declare the total number of variables.
   Default is Arduino UNO total pin number +1.
 */
+#include <RF24Network.h>
+#include <RF24.h>
+#include <SPI.h>
 int chromeDigital[14] = {};
 String fname;
+
+RF24 radio(9,10);                // nRF24L01(+) radio attached using Getting Started board 
+
+RF24Network network(radio);      // Network uses that radio
+const uint16_t hub = 00;    // Address of our node in Octal format ( 04,031, etc)
+const uint16_t player1 = 01;   // Address of the other node in Octal format
+const uint16_t player2 = 02;   // Address of the third node in Octal format
+
+int p1_health = 100;
+int p2_health = 100;
+int damage = 10;
+
+struct payload_t {                 // Structure of our payload
+  unsigned long ms;
+  unsigned long counter;
+  int playerId;
+  bool hit;
+};
 
 void setup() {
   //Do not change the serial connection bitrate.
   Serial.begin(115200);
+  
+  Serial.println("RF24Network/examples/helloworld_rx/");
+ 
+  SPI.begin();
+  radio.begin();
+  network.begin(/*channel*/ 90, /*node address*/ hub);
 }
 
 void loop() {
@@ -37,7 +64,40 @@ void loop() {
   
   
   //Add your code here
+  network.update();                  // Check the network regularly
+
   
+  while ( network.available() ) {     // Is there anything ready for us?
+    
+    RF24NetworkHeader header;        // If so, grab it and print it out
+    payload_t payload;
+    network.read(header,&payload,sizeof(payload));
+    
+    if(payload.playerId == 1 && payload.hit == 1){
+      p1_health = p1_health - damage;
+    } else if (payload.playerId == 2 && payload.hit == 1){
+      p2_health = p2_health - damage;
+    }
+
+    if(p1_health <= 0){
+     Serial.println("Player 2 Wins!"); 
+    } else if(p2_health <= 0){
+     Serial.println("Player 1 Wins!"); 
+    }   
+    
+    Serial.print("Received packet #");
+    Serial.print(payload.counter);
+    Serial.print(" at ");
+    Serial.println(payload.ms);
+    Serial.print("Player ");
+    Serial.println(payload.playerId);
+    Serial.print("Hit ");
+    Serial.println(payload.hit);
+    Serial.print("P1 Health: ");
+    Serial.println(p1_health);
+    Serial.print("P2 Health: ");
+    Serial.println(p2_health);
+  }
   
   //clear the fname to prevent from duplicating functions
   fname = "";
